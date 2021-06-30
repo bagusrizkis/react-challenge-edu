4284a3ece0e346aafa1f24f9b497af7376006e03
bagusrizkis <bagus.tho@gmail.com>
2021-06-30 20:36:30 +0700

    mencoba class component

--- src/App.js
+++ src/App.js
@@ -1,25 +1,83 @@
-import logo from './logo.svg';
-import './App.css';
+import React from 'react'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+    constructor() {
+        super()
+
+        this.state = {
+            message: "Hello React from Vue.js",
+            todos: [],
+            newTodo: '',
+            errors: {
+                isError: false,
+                errMessage: ""
+            }
+        }
+    }
+
+    componentDidUpdate() {
+        // diinvoke ketika ada update
+    }
+
+    componentDidMount() {
+        // diinvoke ketika di render pertam kali
+        this.fetchData()
+    }
+
+    fetchData() {
+        fetch('https://jsonplaceholder.typicode.com/todos')
+            .then(response => response.json())
+            .then(todos => {
+                this.setState({
+                    todos: [...todos]
+                })
+            }).catch(err => {
+                
+            })
+    }
+
+    onChangeHandle(event) {
+        this.setState({
+            newTodo: event.target.value
+        })
+    }
+
+    onSubmitHandle(event) {
+        event.preventDefault()
+        this.setState({
+            todos: [...this.state.todos, {
+                id: this.state.todos[this.state.todos.length-1].id + 1,
+                title: this.state.newTodo
+            }]
+        })
+    }
+
+    render() {
+        return (
+            <div style={{margin: '20px'}}>
+                <h1>{this.state.message}</h1>
+                <form onSubmit={(e) => this.onSubmitHandle(e)}>
+                    <h4>Add Todos:</h4>
+                    <input
+                        type='text'
+                        autoComplete='off'
+                        value={this.state.newTodo}
+                        // onChange={(event) => this.onChangeHandle(event)}
+                        onChange={this.onChangeHandle.bind(this)}
+                    />
+                    <input type="submit" value="Add Todo" />
+                </form>
+                <h4>Todo:</h4>
+                <ul>
+                    {
+                        this.state.todos.map(todo => {
+                            return <li key={todo.id}>{todo.title}</li>
+                        })
+                    }
+                </ul>
+            </div>
+        )
+    }
 }
 
 export default App;
